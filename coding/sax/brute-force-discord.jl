97147f1fe1cd5e82535b95b25c53ce4c62e73ed5
Moritz Konarski <moritz.konarski@gmail.com>
2021-03-22 13:17:37 +0600

    complete first anomalzz detection program

--- coding/sax/brute-force-discord.jl
+++ coding/sax/brute-force-discord.jl
@@ -12,85 +12,200 @@ include("Constants.jl")
 include("Statistics.jl")
 include("ECGReader.jl")
 include("PAA.jl")
+include("SAX.jl")
+include("ESAX.jl")
 include("TSAX.jl")
 include("Distance.jl")
 
 fs = 360
-filepath = "../mitbih/100.mitbih"
-start_index = 78
+filepath = "../mitbih/113.mitbih"
+# filepath = "../mitbih/100.mitbih"
+start_index = 83
 # start_index2 = 371
-start_index2 = 340
+# start_index2 = 340
 # end_index = 720
 c = I
 channels = [None, I, II]
 extract_channels = [false, true, false]
 # w = unsigned(12)
-w = unsigned(48)
-n = unsigned(8)
+# w = unsigned(48)
+# n = unsigned(8)
 
-function get_channel_tsax(start_index::UInt64, length::UInt64)::Tuple{ECGChannel,TSAX}
+function get_channel_tsax(start_index::UInt64, end_index::UInt64, w::UInt64, n::UInt64)::Tuple{ECGChannel,TSAX}
 
-    end_index::UInt64 = start_index + length
+    println("read csv")
+    # end_index::UInt64 = start_index + length
     p = read_csv_file(filepath, fs)
+    # w = (length + 1) ÷ n
 
+    println("read ECG")
     ecg = get_ECG(p, start_index, end_index, channels, extract_channels)
 
+    println("get channel")
     channel = get_ECGChannel(ecg, c)
 
+    println("normalize channel")
     normalized = normalize_ECGChannel(channel)
 
+    println("calculate tpaa")
     tpaa = calculate_tpaa(normalized, w)
+    # println(length(tpaa.data))
 
+    println("calculate tsax")
     tsax = calculate_tsax(tpaa, n)
     return channel, tsax
 end
 
-function get_channel_esax(start_index::UInt64, length::UInt64)::Tuple{ECGChannel,ESAX}
+function get_channel_esax(start_index::UInt64, end_index::UInt64, w::UInt64, n::UInt64)::Tuple{ECGChannel,ESAX}
 
-    end_index::UInt64 = start_index + length
+    println("read csv")
+    # end_index::UInt64 = start_index + length
     p = read_csv_file(filepath, fs)
+    # w = (length + 1) ÷ n
 
+    println("read ECG")
     ecg = get_ECG(p, start_index, end_index, channels, extract_channels)
 
+    println("get channel")
     channel = get_ECGChannel(ecg, c)
 
+    println("normalize channel")
     normalized = normalize_ECGChannel(channel)
 
+    println("calculate epaa")
     epaa = calculate_epaa(normalized, w)
 
+    println("calculate esax")
     esax = calculate_esax(epaa, n)
     return channel, esax
 end
 
-function get_channel_sax(start_index::UInt64, length::UInt64)::Tuple{ECGChannel,SAX}
+function get_channel_sax(start_index::UInt64, end_index::UInt64, w::UInt64, n::UInt64)::Tuple{ECGChannel,SAX}
 
-    end_index::UInt64 = start_index + length
+    println("read csv")
+    # end_index::UInt64 = start_index + length
     p = read_csv_file(filepath, fs)
-    w = (length + 1) ÷ 18
+    # w = (length + 1) ÷ n
 
+    println("read ECG")
     ecg = get_ECG(p, start_index, end_index, channels, extract_channels)
 
+    println("get channel")
     channel = get_ECGChannel(ecg, c)
 
+    println("normalize channel")
     normalized = normalize_ECGChannel(channel)
 
+    println("calculate PAA")
     paa = calculate_paa(normalized, w)
 
+    println("calculate SAX")
     sax = calculate_sax(paa, n)
     return channel, sax
 end
 
 function brute_force(sax::SAX, n::Int64)::Tuple{Float64,Int64}
+    best_dist::Float64 = typemin(Float64)
+    best_location::Int64 = typemax(Int64)
+    l = (length(sax.data) - n + 1)
+    
+    for p in 1:l
+        # println("$p / $l")
+        nearest_dist = typemax(Float64)
+
+        for q in 1:l
+            # println("   $q / $l")
+            if abs(p - q) >= n
+                d = mindist(sub_SAX(sax, p, p+n-1), sub_SAX(sax, q, q+n-1))
+                # println(nearest_dist)
+                if d < nearest_dist
+                    nearest_dist = d
+                end
+            end
+        end
+
+        if nearest_dist > best_dist
+            best_dist = nearest_dist
+            best_location = p
+        end
+    end
+
+    return best_dist, best_location
+end
+
+function brute_force(esax::ESAX, n::Int64)::Tuple{Float64,Int64}
+    best_dist::Float64 = typemin(Float64)
+    best_location::Int64 = typemax(Int64)
+    l = (length(esax.data) - n + 1)
+    
+    for p in 1:l
+        # println("$p / $l")
+        nearest_dist = typemax(Float64)
+
+        for q in 1:l
+            # println("   $q / $l")
+            if abs(p - q) >= n
+                d = mindist(sub_ESAX(esax, p, p+n-1), sub_ESAX(esax, q, q+n-1))
+                # println(nearest_dist)
+                if d < nearest_dist
+                    nearest_dist = d
+                end
+            end
+        end
+
+        if nearest_dist > best_dist
+            best_dist = nearest_dist
+            best_location = p
+        end
+    end
+
+    return best_dist, best_location
+end
+
+function brute_force(tsax::TSAX, n::Int64)::Tuple{Float64,Int64}
+    best_dist::Float64 = typemin(Float64)
+    best_location::Int64 = typemax(Int64)
+    l = (length(tsax.data) - n + 1)
+    
+    for p in 1:l
+        # println("$p / $l")
+        nearest_dist = typemax(Float64)
+
+        for q in 1:l
+            # println("   $q / $l")
+            if abs(p - q) >= n
+                # println("$p $q")
+                d = tsax_dist(sub_TSAX(tsax, p, p+n-1), sub_TSAX(tsax, q, q+n-1))
+                # println(nearest_dist)
+                if d < nearest_dist
+                    nearest_dist = d
+                end
+            end
+        end
+
+        if nearest_dist > best_dist
+            best_dist = nearest_dist
+            best_location = p
+        end
+    end
+
+    return best_dist, best_location
+end
+
+function brute_force(channel::ECGChannel, n::Int64)::Tuple{Float64,Int64}
     best_dist::Float64 = 0
     best_location::Int64 = typemax(Int64)
+    l = (length(channel.data) - n + 1)
     
-    for p in 1:(length(sax.data) - n + 1)
+    for p in 1:l
+        # println("$p / $l")
         nearest_dist = typemax(Float64)
-        for q in 1:(length(sax.data) - n + 1)
+
+        for q in 1:l
             if abs(p - q) >= n
-                cdist = mindist(sub_SAX(sax, p, p+n-1), sub_SAX(sax, q, q+n-1))
-                if cdist < nearest_dist
-                    nearest_dist = cdist
+                d = euclidean_distance(sub_ECGChannel(channel, p, p+n-1), sub_ECGChannel(channel, q, q+n-1))
+                if d < nearest_dist
+                    nearest_dist = d
                 end
             end
         end
@@ -105,12 +220,63 @@ function brute_force(sax::SAX, n::Int64)::Tuple{Float64,Int64}
 end
 
 function sub_SAX(s::SAX, start::Int64, stop::Int64)::SAX
+    # TODO: add safety checks
     return SAX(s.name, s.sampling_frequency, start, stop, s.w, s.data[start:stop], s.alphabet, s.breakpoints)
 end
 
+function sub_ESAX(s::ESAX, start::Int64, stop::Int64)::ESAX
+    # TODO: add safety checks
+    return ESAX(s.name, s.sampling_frequency, start, stop, s.w, s.data[start:stop], s.alphabet, s.breakpoints)
+end
 
-c1, s1 = get_channel_sax(unsigned(start_index), unsigned(900*720-1))
-c1, s1 = get_channel_esax(unsigned(start_index), unsigned(900*720-1))
-c1, s1 = get_channel_tsax(unsigned(start_index), unsigned(900*720-1))
+function sub_TSAX(s::TSAX, start::Int64, stop::Int64)::TSAX
+    # TODO: add safety checks
+    # println(start)
+    # println(stop)
+    return TSAX(s.name, s.sampling_frequency, start, stop, s.w, s.data[start:stop], s.bits[((start-1)*length(s.data) ÷ s.w+1):(stop*length(s.data) ÷ s.w)] , s.alphabet, s.breakpoints)
+end
 
-# brute_force(s1, 360)
+function sub_ECGChannel(c::ECGChannel, start::Int64, stop::Int64)::ECGChannel
+    # TODO: add safety checks
+    return ECGChannel(c.name, c.sampling_frequency, start, stop, c.data[start:stop])
+end
+
+length_factor = 15
+end_index = unsigned(length_factor*720+start_index-1)
+# w = unsigned(length_factor * 36)
+w = unsigned(length_factor * 18)
+# n = unsigned(9)
+n = unsigned(7)
+
+c1, s1 = get_channel_sax(unsigned(start_index), end_index, w, n)
+
+_, s2 = get_channel_esax(unsigned(start_index), end_index, w, n)
+
+_, s3 = get_channel_tsax(unsigned(start_index), end_index, w, n)
+
+println("brute force sax")
+a, b = brute_force(s1, 8)
+println((a, b, b * 320/8))
+
+println("brute force esax")
+c, d = brute_force(s2, 8*3)
+println((c, d, d * 320/8))
+
+println("brute force tsax")
+e, f = brute_force(s3, 8)
+println((e, f, f * 320/8))
+
+println("brute force euclid")
+println(brute_force(c1, 320))
+
+# (2.5607469613376486, 1726)
+# (10.037811016352121, 8372)
+
+# brute force sax
+# (5.582262406533327, 35, 1400.0)
+# brute force esax
+# (38.286588601218014, 31, 1240.0)
+# brute force tsax
+# (6.2851159496436555, 35, 1400.0)
+# brute force euclid
+# (5.650161502116553, 1340)
--- coding/sax/sax-segment-plotting.jl
+++ coding/sax/sax-segment-plotting.jl
@@ -10,61 +10,89 @@ GitHub: @moritz-konarski (https://github.com/moritz-konarski)
 
 include("Constants.jl")
 include("ECGReader.jl")
+include("Statistics.jl")
 include("PAA.jl")
 include("SAX.jl")
 
 using Plots
 
-factor = 1
+factor = 5
 fs = 360
-filepath = "../mitbih/100.mitbih"
-start_index = 1
-end_index = factor*720
+filepath = "../mitbih/113.mitbih"
+# filepath = "../mitbih/100.mitbih"
+offset  = 10*720
+start_index = 1 + offset
+end_index = factor*720 + offset
 c = I
 channels = [None, I, II]
 extract_channels = [false, true, false]
 # w = unsigned(factor*40)
-w = unsigned(factor*16)
+w = unsigned(factor*18)
 n = unsigned(7)
 
+println("read csv file")
 p = read_csv_file(filepath, fs)
 
+println("extract ECG data")
 ecg = get_ECG(p, start_index, end_index, channels, extract_channels)
 
+println("extract single channel data")
 channel = get_ECGChannel(ecg, c)
 
+println("normalize ECG channel")
 normalized = normalize_ECGChannel(channel)
 
+println("calulate PAA")
 paa = calculate_paa(normalized, w)
 
-p1 = plot(channel.data)
-xlabel!(p1, "raw")
+sax = calculate_sax(paa, n)
 
-p1 = plot(normalized.data)
+# p1 = plot(channel.data)
+# xlabel!(p1, "raw")
+
+println("plot normalized data")
+xs = start_index:end_index
+p1 = plot(xs, normalized.data)
 # xlabel!(p2, "normalized")
 
+println("plot segments data")
 ys = repeat(paa.data, inner=2)
 
 xs = zeros(Float64, length(ys))
 for (i, y) in enumerate(ys)
     if i % 2 == 1
-        xs[i] = (i-1) * (paa.end_index - paa.start_index) / paa.w / 2
+        xs[i] = (i-1) * (paa.end_index - paa.start_index) / paa.w / 2 + offset
     else
-        xs[i] = (i) * (paa.end_index - paa.start_index) / paa.w / 2
+        xs[i] = (i) * (paa.end_index - paa.start_index) / paa.w / 2 + offset
     end
 end
 
 plot!(p1, xs, ys)
 # plot!(p1, xs, ys, seriestype = :scatter)
 
+
+println("plot and calculate sax")
 ys = paa.data
 
 xs = zeros(Float64, length(ys))
 for (i, y) in enumerate(ys)
-    xs[i] = (i-0.5) * (paa.end_index - paa.start_index) / paa.w
+    xs[i] = (i-0.5) * (paa.end_index - paa.start_index) / paa.w + offset
 end
 
-plot!(p1, xs, ys, seriestype = :scatter, series_annotations = text.(calculate_sax(paa, n).data, :bottom))
-# ylims!(-2, 3)
+plot!(p1, xs, ys, seriestype = :scatter, series_annotations = text.(sax.data, :bottom))
+
+# plot!(p1, [1400; 1400], [6; -2.5], color="black", label="sax and tsax")
+# plot!(p1, [1400+8*40; 1400+8*40], [6; -2.5], color="black")
+
+
+# plot!(p1, [1240; 1240], [6; -2.5], color="red", label="esax")
+# plot!(p1, [1240+8*40; 1240+8*40], [6; -2.5], color="red")
+
+# plot!(p1, [1340; 1340], [6; -2.5], color="green", label="euclid")
+# plot!(p1, [1340+8*40; 1340+8*40], [6; -2.5], color="green")
+
+
+# xlims!(start_index, end_index)
+ylims!(-2, 6)
 
 # savefig("./test.png")
