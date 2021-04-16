f7eb5438f15bf4b118aab18aa2b8fb8ed502386f
Moritz Konarski <moritz.konarski@gmail.com>
2021-04-16 15:44:08 +0600

    properly scan ecgs and start digitizing

--- coding/digitizing/Interpolate.jl
+++ coding/digitizing/Interpolate.jl
@@ -1,25 +1,44 @@
 using CSV, Tables, Plots, Interpolations, StatsBase, DataFrames
 
-filename1 = "./V3_97bpm"
-filename2 = "./V3_70bpm"
-filename3 = "./gimp_test"
-path = filename2
+f1 = "./scans/070bpm-v3"
+f2 = "./scans/088bpm-v3"
+f3 = "./scans/092bpm-v3"
+f4 = "./scans/097bpm-v3"
 
-N_interpolation_points = 1000
+files = [f1, f2, f3, f4]
 
-data = CSV.File("$path.csv") |> Tables.matrix
+N_interpolation_points = Int64(19 * 1//5 * 500)
 
-i = interpolate(data[:, 2], BSpline(Linear()))
-interpolated = i(range(1, length(i), length = N_interpolation_points))
+function interpol(path::String; plot=false)
+    data = CSV.File("$path.csv") |> Tables.matrix
+    println(size(data))
 
-interpolated = standardize(ZScoreTransform, interpolated, dims = 1)
+    dmax = maximum(data[:, 1])
+    dmin = minimum(data[:, 1])
+    nodes = (data[:, 1],)
 
-df = DataFrame(sample = collect(1:N_interpolation_points), V3 = interpolated)
-show(df)
+    i = interpolate(nodes, data[:, 2], Gridded(Linear()))
+# i = interpolate(data[:, 2], BSpline(Linear()))
+    interpolated = i(range(dmin, dmax, length = N_interpolation_points))
 
-CSV.write("$path-int-norm.csv", df)
+    interpolated = standardize(ZScoreTransform, interpolated, dims = 1)
 
-p1 = plot(data[:, 2])
-p2 = plot(interpolated)
-plot(p1, p2, layout = (2, 1))
+    df = DataFrame(sample = collect(1:N_interpolation_points), V3 = interpolated)
+
+#show(df)
+#println()
+
+    CSV.write("$path-int-norm.csv", df)
+
+    if plot
+        p1 = plot(data[:, 2], title = "raw")
+        p2 = plot(interpolated, title = "interpolated")
+        plot(p1, p2, layout = (2, 1))
+    end
+end
+
+for file in files
+    interpol(file)
+end
+
 
