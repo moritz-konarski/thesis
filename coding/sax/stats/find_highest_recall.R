4f5541a864546afa8249566bf96500392692ac2f
Moritz Konarski <moritz.konarski@gmail.com>
2021-05-23 15:00:11 +0600

    change something in analysis script

--- coding/sax/stats/find_highest_recall.R
+++ coding/sax/stats/find_highest_recall.R
@@ -77,8 +77,50 @@ cor(s2$recall, s2$subsequence)
 data <- read.csv2("./data/no_sub-single_sax-summary_msax.csv")
 # 
 s1 <- data[data$recall >= 0.95, ]
-s1 <- s1[order(s1$precision, decreasing = T), ]
+s1 <- s1[order(s1$precision, decreasing = T), ][1:10, ]
 
+data <- read.csv2("./data/no_sub-single_sax-summary_sax.csv")
+
+s2 <- data[data$recall >= 0.95, ]
+s2 <- s2[order(s2$precision, decreasing = T), ][1:10, ]
+
+data <- read.csv2("./data/no_sub-dual_sax-summary_sax.csv")
+
+s3 <- data[data$recall >= 0.95, ]
+s3 <- s3[order(s3$precision, decreasing = T), ][1:10, ]
+
+ss <- read.csv2("./real_test_1/data-beat_types-FALSE--single-FALSE--first_sax-FALSE.csv")
+
+ss1 <- ss[ss$is_msax == 1, ]
+ss1 <- ss1[ss1$k == -1 & ss1$paa_segment_count == 6 & ss1$alphabet_size == 24, ]
+
+ss2 <- ss[ss$is_msax == 0, ]
+ss2 <- ss2[ss2$k == -1 & ss2$paa_segment_count == 12 & ss2$alphabet_size == 22, ]
+
+ss3 <- ss[ss$k == -1 & ss$paa_segment_count == 6 & ss$alphabet_size == 24 & ss$is_msax==0, ]
+
+p <- rbind(ss1, ss2)
+q <- rbind(ss1, ss3)
+
+boxplot(recall ~ as.factor(method), 
+        data=p, 
+        main = "Boxplot Comparing Recall for Dual SAX and MSAX",
+        xlab = "Representation",
+        ylab = "Recall"
+    )
+
+cor(p$recall, p$is_msax)
+cor.test(p$recall, p$is_msax)
+
+boxplot(precision ~ as.factor(method), 
+        data=p, 
+        main = "Boxplot Comparing Precision for Dual SAX and MSAX",
+        xlab = "Representation",
+        ylab = "Precision"
+    )
+
+cor(p$precision, p$is_msax)
+cor.test(p$precision, p$is_msax)
 
 
 # https://www.tutorialspoint.com/r/r_boxplots.htm
